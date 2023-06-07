96cce191b99dee1f1df67a07277f2134a6a7d1e8
Florian Schubert <florian.schubert.sg@gmail.com>
2023-06-07 22:49:11 +0200

    Moved Firefox, tree to System. Added lsd

--- configuration.nix
+++ configuration.nix
@@ -19,8 +19,6 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # grub
@@ -39,20 +37,12 @@
     };
   };
   
-  # boot.kernelParams = [ "processor.max_cstate=4" "amd_iomu=soft" "idle=nowait" ];
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
-
+  # Networking
   networking.hostName = "Kakariko"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -62,32 +52,14 @@
     # useXkbConfig = true; # use xkbOptions in tty.
   };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User Account
   users.users.florian = {
     isNormalUser = true;
     initialPassword = "";
-    extraGroups = [ "wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      firefox
-      tree
-    ];
+    extraGroups = [ "wheel" "networkmanager"];
   };
 
   # List packages installed in system profile. To search, run:
@@ -98,6 +70,9 @@
     git
     starship
     wayfire
+    firefox
+    lsd
+    tree
     home-manager
   ];
 
@@ -127,6 +102,8 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
+
+  # 
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = false;
