e484e86c45105f87de10cfd9ca15103c7e308433
Florian Schubert <florian.schubert.sg@gmail.com>
2023-06-18 18:58:59 +0200

    Removed xdg config due to it beeing already in the module

--- configuration.nix
+++ configuration.nix
@@ -83,7 +83,6 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    xdg-desktop-portal-hyprland
     kitty
     wget
     git
