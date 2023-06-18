5e02e540a65ea279d7b00815fc4285b0e35b1dac
Florian Schubert <florian.schubert.sg@gmail.com>
2023-06-18 17:20:57 +0200

    Added Kitty

--- configuration.nix
+++ configuration.nix
@@ -83,6 +83,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    kitty
     wget
     git
     starship
