0b3f840dd833a4964fef561d401f0b6e2b6ec209
Florian Schubert <florian.schubert.sg@gmail.com>
2023-06-19 14:10:13 +0200

    Added qt support

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    libsForQt5.qt5.qtwayland
+    qt6.qtwayland
     kitty
     wget
     git
