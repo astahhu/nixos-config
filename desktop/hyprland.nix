456b928a5ff742064239ab1491785eb5fbd4d545
Florian Schubert <florian.schubert.sg@gmail.com>
2023-07-09 16:56:06 +0200

    Configured Gtk

--- desktop/hyprland.nix
+++ desktop/hyprland.nix
@@ -1,5 +1,20 @@
-{config, pkgs, hyprland-contrib,...} : {
+{config, pkgs, hyprland-contrib,...}:
 
+  let configure-gtk = pkgs.writeTextFile {
+      name = "configure-gtk";
+      destination = "/bin/configure-gtk";
+      executable = true;
+      text = let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+      in ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+        gnome_schema=org.gnome.desktop.interface
+        gsettings set $gnome_schema gtk-theme 'Dracula'
+        '';
+  };
+
+  in {
   programs.hyprland = {
     enable = true;
     xwayland = {
@@ -10,15 +25,19 @@
   };
 
   environment.systemPackages = with pkgs; [
+    configure-gtk
     swaylock
     libsForQt5.qt5.qtwayland
     hyprpicker
     hyprland-contrib.packages.${system}.grimblast
     qt6.qtwayland
+    glib
     kitty # Terminal Emulator
     firefox
     grim # Screenshots
     slurp # Select Screen Area for Screenschots etc.
+    dracula-theme # gtk theme
+    gnome3.adwaita-icon-theme
   ];
   
 }
