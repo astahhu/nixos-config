f56bfe7334ee000d00981ed0b2d4056d009707c9
Florian Schubert <florian.schubert.sg@gmail.com>
2023-09-25 00:29:32 +0200

    Added Stylix Bacjground Image

--- desktop/gnome.nix
+++ desktop/gnome.nix
@@ -2,7 +2,12 @@
   services.xserver.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
+  
   hardware.pulseaudio.enable = false;
+    stylix.image = pkgs.fetchurl {
+    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+    sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  };
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/monokai.yaml";
   stylix.fonts.monospace = {
       package = (pkgs.nerdfonts.override { fonts = [
