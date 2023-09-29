b01ae89a207a65e5568c8bb4bbf7a856e51091c3
Florian Schubert <florian.schubert.sg@gmail.com>
2023-09-29 10:12:37 +0200

    Added OpenCl Support

--- configuration.nix
+++ configuration.nix
@@ -95,6 +95,7 @@
 
   programs.fish.enable = true;
   users.defaultUserShell = pkgs.fish;
+  services.xmr-stak.openclSupport = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
