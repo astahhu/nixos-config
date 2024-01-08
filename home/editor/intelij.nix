996a474b2d1a29ecda6dc7b474048b4db359b271
florian <florian.schubert.sg@gmail.com>
2024-01-08 22:37:05 +0100

    Added Copilto to Jetbrains IDEs

--- home/editor/intelij.nix
+++ home/editor/intelij.nix
@@ -1,6 +1,10 @@
 { pkgs, ...} : {
   home.packages = with pkgs; [
-    jetbrains.idea-ultimate
-    jetbrains.rust-rover
+    (jetbrains.plugins.addPlugins 
+      jetbrains.idea-ultimate [ "github-copilot" "nixidea" ])
+    (jetbrains.plugins.addPlugins 
+      jetbrains.rust-rover [ "github-copilot" ])
   ];
+
+
 }
