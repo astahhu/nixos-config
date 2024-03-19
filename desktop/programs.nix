57d83014bc564447acc2341fe90fd95f4b11b7cc
Florian Schubert <florian.schubert.sg@gmail.com>
2024-03-19 13:54:04 +0100

    Updated Only Office

--- desktop/programs.nix
+++ desktop/programs.nix
@@ -1,7 +1,7 @@
 { pkgs, ...} : {
   programs.firefox.enable = true;
   environment.systemPackages = with pkgs; [
-    onlyoffice-bin_7_5
+    onlyoffice-bin_latest
     bootstrap-studio
     thunderbird
   ];
