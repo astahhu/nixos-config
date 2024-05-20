8514f59564418b58da80a4ff80aa0144150342f0
florian <florian.schubert.sg@gmail.com>
2024-05-20 23:04:44 +0200

    Fixed impermant paths

--- hosts/hateno/configuration.nix
+++ hosts/hateno/configuration.nix
@@ -26,11 +26,11 @@
   environment.persistence."/fast_persistent" = {
     hideMounts = true;
     files = [
-      /etc/machine-id
-      /etc/ssh/ssh_host_ed25519_key.pub
-      /etc/ssh/ssh_host_ed25519_key
-      /etc/ssh/ssh_host_rsa_key
-      /etc/ssh/ssh_host_rsa_key.pub
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_rsa_key" 
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
   };
 
