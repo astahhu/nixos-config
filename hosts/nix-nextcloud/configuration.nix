7b692e536d167e05db69d9e5b9b1ffe302534dcf
florian <florian.schubert.sg@gmail.com>
2024-06-30 18:43:51 +0200

    Updated Nextcloud to 29

--- hosts/nix-nextcloud/configuration.nix
+++ hosts/nix-nextcloud/configuration.nix
@@ -26,7 +26,7 @@
   # services.nextcloud.database.createLocally = true;
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud28;
+    package = pkgs.nextcloud29;
     hostName = "nextcloud.astahhu.de";
     https = true;
     configureRedis = true;
