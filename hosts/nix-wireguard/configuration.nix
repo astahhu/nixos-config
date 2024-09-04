c7a72c837b441a17279d0c8da87620e0abd0ac87
florian <florian.schubert.sg@gmail.com>
2024-09-04 14:28:32 +0200

    add(nix-wireguard): setup wiregurad

--- hosts/nix-wireguard/configuration.nix
+++ hosts/nix-wireguard/configuration.nix
@@ -13,7 +13,142 @@
   networking.hostName = "nix-wireguard";
 
   # Uncomment if you need Secrets for this Hosts, AFTER the first install  
-  # sops.defaultSopsFile = ../../secrets/nix-wireguard.yaml;
+  sops.defaultSopsFile = ../../secrets/nix-wireguard.yaml;
+  sops.secrets.wireguard_private = {};
+
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "ens192";
+  networking.nat.internalInterfaces = [ "wg0" ];
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 ];
+  };
+
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the server's end of the tunnel interface.
+      ips = [ "10.105.42.1/24" ];
+
+      # The port that WireGuard listens to. Must be accessible by the client.
+      listenPort = 51820;
+
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.105.42.0/24 -o ens192 -j MASQUERADE
+      '';
+
+      # This undoes the above command
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.105.42.0/24 -o ens192 -j MASQUERADE
+      '';
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "config.sops.secrets.wireguard.path";
+
+      peers = [
+        # List of allowed peers.
+        { # Feel free to give a meaning full name
+          # Public key of the peer (not a file path).
+          publicKey = "qAzWfOAMP3w4vusIyjyHFl7aTLG7eZrCz20bE6PkVik=`";
+          # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+          allowedIPs = [ "10.105.42.2/32" ];
+        }
+        {
+          publicKey = "X7SwpnOfNBFyXo5ELD7Jfd4psSu/WiF6ucyNP+zvrww=";
+          allowedIPs = [ "10.105.42.3/32" ];
+        }
+        {
+          publicKey = "/+DAqTxrCYqicFmJ3hGPc4++BwBbkni7MH5BNOKuinc=";
+          allowedIPs = [ "10.105.42.4/32" ];
+        }
+        {
+          publicKey = "Xt92xMbkfhiB63Yig3caZuPs7geA53LdCwFwCZjR/y0=";
+          allowedIPs = [ "10.105.42.5/32" ];
+	  endpoint = "134.99.39.12:60538";
+        }
+        {
+          publicKey = "Cxjqdj+sbnz1reh0INfylzyhkm18zjWgg3P6BOC+DW0=";
+          allowedIPs = [ "10.105.42.6/32" ];
+        }
+        {
+          publicKey = "uOKm/Gdqn7dGMYlzYuVB9LP2U2peLP6qqHP8neUNwEU=";
+          allowedIPs = [ "10.105.42.7/32" ];
+        }
+        {
+          publicKey = "zsqHwvlfiPVuPMVKoIeN1h0CE5ts9H/numnOIrNJZlk=";
+          allowedIPs = [ "10.105.42.8/32" ];
+        }
+        {
+          publicKey = "1FmBk72xRgoFxSBaeaxeHWBzlk+mKFAUzDnUUiGpKUo=";
+          allowedIPs = [ "10.105.42.9/32" ];
+        }
+        {
+          publicKey = "KdimHNVz4OmMlc+ZUz06ntBLdB6fw+lC4RmdWXDt00U=";
+          allowedIPs = [ "10.105.42.10/32" ];
+        }
+        {
+          publicKey = "t0hmDrrymNCcmkcaBpfEUWUBdJ2sOdexHFjwbJzIMHo=";
+          allowedIPs = [ "10.105.42.11/32" ];
+        }       
+	{
+          publicKey = "mLZAKxex044RCUbIAiOC8LdlHyYvp+CVeciHRLZLDgE=";
+          allowedIPs = [ "10.105.42.12/32" ];
+        }
+        {
+          publicKey = "hC4d0h2ewwWFdHeKBRGJupR1Qm1pSv832rkv8K4xpgA=";
+          allowedIPs = [ "10.105.42.13/32" ];
+        }
+        {
+          publicKey = "4OV6hnXsTDdmF0hj0IyAyPg9fwJAOhqHEv04A9ZiFlQ=";
+          allowedIPs = [ "10.105.42.14/32" ];
+        }
+        {
+          publicKey = "LOJoxvbNiRZ3/EAd10kPolzcGb2VeMf1lAVTeiMriyU=";
+          allowedIPs = [ "10.105.42.15/32" ];
+        }       {
+          publicKey = "TTWhoxuCOJryuTfMcQ+7mWxpbYtOngfyqbK0LCF+qE0=";
+          allowedIPs = [ "10.105.42.16/32" ];
+        }
+        {
+          publicKey = "GMzQnVABjufZQ6Czm6D+X85C5qr9Y2FYF3fWMg313QM=";
+          allowedIPs = [ "10.105.42.17/32" ];
+        }
+        {
+          publicKey = "zZIOUcr6TTISKLSe0RzFpWyghKGD0PTsr5WlcRBl3xQ=";
+          allowedIPs = [ "10.105.42.18/32" ];
+        }
+        {
+          publicKey = "mNeA/lJCOVO78BxPiXhwbBjLAZwQ90NNMPDdf1Z3v24=";
+          allowedIPs = [ "10.105.42.19/32" ];
+        }
+        {
+          publicKey = "nJBfaylAfoTZjYco5ZgJusm60XOBCKzFeK30yY3e41k=";
+          allowedIPs = [ "10.105.42.20/32" ];
+        }
+        {
+          publicKey = "/hTh57oidbhGEWViahL4dxhCLxXQ/q0I8MIlZ7go/1E=";
+          allowedIPs = [ "10.105.42.21/32" ];
+        }
+        {
+          publicKey = "2ytpT/rSMOv0xpukKm5BL1ipDyv9MG8wqwxLG0790yE=";
+          allowedIPs = [ "10.105.42.22/32" ];
+        }
+        {
+          publicKey = "L235h97SbeoYKE25VzkJp7uilmQ3VJzGLZrr3SKbIHg=";
+          allowedIPs = [ "10.105.42.23/32" ];
+        }
+        {
+          publicKey = "ri3E91KHp15VrgdVSdHBQimb97DEQuDu8SiQ3SODB1I=";
+          allowedIPs = [ "10.105.42.24/32" ];
+        }
+      ];
+    };
+  };
 
   # Networking
   networking.firewall.enable = true;
