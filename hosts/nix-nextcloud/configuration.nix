cfe13b6d73237425d01470a9b7e5ebf3d66246a9
florian <florian.schubert.sg@gmail.com>
2024-11-14 16:04:29 +0100

    nextcloud: Added CODE Server

--- hosts/nix-nextcloud/configuration.nix
+++ hosts/nix-nextcloud/configuration.nix
@@ -1,13 +1,12 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-} : {
+{ config
+, lib
+, pkgs
+, inputs
+, ...
+}: {
 
   astahhu.common = {
     is_server = true;
@@ -17,16 +16,16 @@
       device = "/dev/sda";
     };
   };
- 
+
   # Networking
   networking.nat = {
     enable = true;
-    internalInterfaces = ["ve-+"];
+    internalInterfaces = [ "ve-+" ];
     externalInterface = "ens18";
     # Lazy IPv6 connectivity for the container
     enableIPv6 = true;
   };
-  
+
   networking.hostName = "nix-nextcloud"; # Define your hostname.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
   sops.defaultSopsFile = ../../secrets/nix-nextcloud.yaml;
@@ -35,24 +34,52 @@
     format = "binary";
   };
 
-  sops.secrets.dockerproxy_env = {};
+  sops.secrets.dockerproxy_env = { };
 
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
+  virtualisation.oci-containers.backend = "docker";
+
+  virtualisation.oci-containers.containers."collabora" = {
+    image = "collabora/code:latest";
+    environment = {
+      aliasgroup1 = "https://cloud.astahhu.de:443";
+      server_name = "collabora.astahhu.de";
+      extra_params = "--o:ssl.enable=true --o:remote_font_config.url=https://cloud.astahhu.de/apps/richdocuments/settings/fonts.json";
+    };
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.collabora.entrypoints" = "websecure";
+      "traefik.http.routers.collabora.rule" = "Host(`collabora.astahhu.de`)";
+      "traefik.http.routers.collabora.tls" = "true";
+      "traefik.http.routers.collabora.tls.certresolver" = "letsencrypt";
+      "traefik.http.services.collabora.loadbalancer.server.port" = "9980";
+      "traefik.http.services.collabora.loadbalancer.server.scheme" = "https";
+      "traefik.http.services.collabora.loadbalancer.serversTransport" = "collabora@file";
+    };
+  };
+
+  services.traefik.dynamicConfigOptions.http.serversTransports.collabora.insecureSkipVerify = true;
+
   # List services that you want to enable:
   nix-tun = {
     services.traefik = {
       enable = true;
+      enable_docker = true;
       letsencryptMail = "it@asta.hhu.de";
     };
     services.containers.nextcloud = {
       enable = true;
       hostname = "cloud.astahhu.de";
-      extraTrustedProxies = ["134.99.154.202" "192.168.100.10" "192.168.100.11"];
+      extraTrustedProxies = [ "134.99.154.202" "192.168.100.10" "192.168.100.11" "127.0.0.1" ];
     };
   };
+
+  services.traefik.staticConfigOptions.entryPoints.web.proxyProtocol.insecure = true;
+
+  services.traefik.staticConfigOptions.entryPoints.websecure.proxyProtocol.insecure = true; #.trustedIPs = [ "192.168.0.0/16" "127.0.0.1" ];
 
   containers.nextcloud = {
     bindMounts.docker = {
@@ -65,7 +92,7 @@
       mountPoint = "/etc/resolv.conf";
     };
 
-    config = { ... } : {
+    config = { ... }: {
       environment.systemPackages = [
         pkgs.docker
       ];
@@ -73,8 +100,28 @@
       services.nextcloud.settings.default_phone_region = "DE";
       services.nextcloud.notify_push = {
         dbuser = lib.mkForce "nextcloud";
-	dbhost = lib.mkForce "localhost:/run/mysqld/mysqld.sock";
+        dbhost = lib.mkForce "localhost:/run/mysqld/mysqld.sock";
       };
+
+      services.nginx.virtualHosts.nextcloud.extraConfig = lib.mkForce ''
+        index index.php index.html /index.php$request_uri;
+        add_header X-XSS-Protection "1; mode=block" always;
+        add_header X-Robots-Tag "noindex, nofollow" always;
+        add_header X-Download-Options noopen always;
+        add_header X-Permitted-Cross-Domain-Policies none always;
+        add_header X-Frame-Options sameorigin always;
+        add_header Referrer-Policy no-referrer always;
+        add_header Strict-Transport-Security "max-age=${toString config.services.nextcloud.nginx.hstsMaxAge}; includeSubDomains" always;
+        client_max_body_size ${config.services.nextcloud.maxUploadSize};
+        fastcgi_buffers 64 4K;
+        fastcgi_hide_header X-Powered-By;
+        gzip on;
+        gzip_vary on;
+        gzip_comp_level 4;
+        gzip_min_length 256;
+        gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
+        gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
+      '';
     };
   };
 
@@ -115,25 +162,25 @@
 
   security.pki.certificates = [
     ''
-    -----BEGIN CERTIFICATE-----
-    MIIDFTCCAf2gAwIBAgIUOHwthMO3Dw04xRtE3/P9tzcGAc4wDQYJKoZIhvcNAQEL
-    BQAwGTEXMBUGA1UEAwwOMTkyLjE2OC4xMDAuMTAwIBcNMjQxMDE3MTM1NTU3WhgP
-    MzAyNDAyMTgxMzU1NTdaMBkxFzAVBgNVBAMMDjE5Mi4xNjguMTAwLjEwMIIBIjAN
-    BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAobtxgNLyzT1KjhVNmAJil0b4AxhV
-    xfRO5/QvF+ehykVKdB4C8yNv2PSjYA6JDflmYq0if0DoA0fW4znGem4/2wbRL+B/
-    dGiGUTvWnVv6sdPy7kArS8Q+z6b4R56VnDYvqN6N6ADLnCChur0rYu3F0H+vezZx
-    JRRg44Lzpw6KesyrE2YSd8vsdNjMJgu+35RZm91pZxrVzeQyHongTdQRtuabUq55
-    FyXvflbeAwpRTKAXGMxBXsZ7NgsCVm5EX9O0M0tw2Vy3TVAvh4rFAN89t7kIbsOx
-    EAMcMlmTqPr8rvoshh4w0mTP45ON489799/o5vLCLLYHoNkiwZDNbx48twIDAQAB
-    o1MwUTAdBgNVHQ4EFgQU4tteG/vGAU1gbQ6s5O31sRKyQzkwHwYDVR0jBBgwFoAU
-    4tteG/vGAU1gbQ6s5O31sRKyQzkwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0B
-    AQsFAAOCAQEADz1sh6ipSNfbd3rE7hPRG1TN03Atmlq8yJ3uTsAGB7BDmPlad4fq
-    Ak+MrHuqDu0+iYEaOk/qcr0kP6ozBoorahA5GXO+qV1I9YlZH5dccC9RnffyksKT
-    T2epsJL91b9rh5bhUh4pBnQsRdpX8A7479DHNUP0SNYtrCW/vaeJuVbh9zU/VrLQ
-    vZ6Nk04h4X00M10C1v0hCFRJqKPXpUW96SJAmrM7F5TAiMFeO6zeGCoXWpaq7M5X
-    HwORa5h8YfdSozsB2XCDVd/0euDa5BOCBM5O431PhwL3peAwEeaGlXEk1XXnLDQ5
-    ylufmUaJuk6YltROWkNq/821NljZR5j4Nw==
-    -----END CERTIFICATE-----
+      -----BEGIN CERTIFICATE-----
+      MIIDFTCCAf2gAwIBAgIUOHwthMO3Dw04xRtE3/P9tzcGAc4wDQYJKoZIhvcNAQEL
+      BQAwGTEXMBUGA1UEAwwOMTkyLjE2OC4xMDAuMTAwIBcNMjQxMDE3MTM1NTU3WhgP
+      MzAyNDAyMTgxMzU1NTdaMBkxFzAVBgNVBAMMDjE5Mi4xNjguMTAwLjEwMIIBIjAN
+      BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAobtxgNLyzT1KjhVNmAJil0b4AxhV
+      xfRO5/QvF+ehykVKdB4C8yNv2PSjYA6JDflmYq0if0DoA0fW4znGem4/2wbRL+B/
+      dGiGUTvWnVv6sdPy7kArS8Q+z6b4R56VnDYvqN6N6ADLnCChur0rYu3F0H+vezZx
+      JRRg44Lzpw6KesyrE2YSd8vsdNjMJgu+35RZm91pZxrVzeQyHongTdQRtuabUq55
+      FyXvflbeAwpRTKAXGMxBXsZ7NgsCVm5EX9O0M0tw2Vy3TVAvh4rFAN89t7kIbsOx
+      EAMcMlmTqPr8rvoshh4w0mTP45ON489799/o5vLCLLYHoNkiwZDNbx48twIDAQAB
+      o1MwUTAdBgNVHQ4EFgQU4tteG/vGAU1gbQ6s5O31sRKyQzkwHwYDVR0jBBgwFoAU
+      4tteG/vGAU1gbQ6s5O31sRKyQzkwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0B
+      AQsFAAOCAQEADz1sh6ipSNfbd3rE7hPRG1TN03Atmlq8yJ3uTsAGB7BDmPlad4fq
+      Ak+MrHuqDu0+iYEaOk/qcr0kP6ozBoorahA5GXO+qV1I9YlZH5dccC9RnffyksKT
+      T2epsJL91b9rh5bhUh4pBnQsRdpX8A7479DHNUP0SNYtrCW/vaeJuVbh9zU/VrLQ
+      vZ6Nk04h4X00M10C1v0hCFRJqKPXpUW96SJAmrM7F5TAiMFeO6zeGCoXWpaq7M5X
+      HwORa5h8YfdSozsB2XCDVd/0euDa5BOCBM5O431PhwL3peAwEeaGlXEk1XXnLDQ5
+      ylufmUaJuk6YltROWkNq/821NljZR5j4Nw==
+      -----END CERTIFICATE-----
     ''
     ''
             -----BEGIN CERTIFICATE-----
@@ -163,25 +210,25 @@
       -----END CERTIFICATE-----
     ''
     ''
-    -----BEGIN CERTIFICATE-----
-    MIIDFTCCAf2gAwIBAgIUOHwthMO3Dw04xRtE3/P9tzcGAc4wDQYJKoZIhvcNAQEL
-    BQAwGTEXMBUGA1UEAwwOMTkyLjE2OC4xMDAuMTAwIBcNMjQxMDE3MTM1NTU3WhgP
-    MzAyNDAyMTgxMzU1NTdaMBkxFzAVBgNVBAMMDjE5Mi4xNjguMTAwLjEwMIIBIjAN
-    BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAobtxgNLyzT1KjhVNmAJil0b4AxhV
-    xfRO5/QvF+ehykVKdB4C8yNv2PSjYA6JDflmYq0if0DoA0fW4znGem4/2wbRL+B/
-    dGiGUTvWnVv6sdPy7kArS8Q+z6b4R56VnDYvqN6N6ADLnCChur0rYu3F0H+vezZx
-    JRRg44Lzpw6KesyrE2YSd8vsdNjMJgu+35RZm91pZxrVzeQyHongTdQRtuabUq55
-    FyXvflbeAwpRTKAXGMxBXsZ7NgsCVm5EX9O0M0tw2Vy3TVAvh4rFAN89t7kIbsOx
-    EAMcMlmTqPr8rvoshh4w0mTP45ON489799/o5vLCLLYHoNkiwZDNbx48twIDAQAB
-    o1MwUTAdBgNVHQ4EFgQU4tteG/vGAU1gbQ6s5O31sRKyQzkwHwYDVR0jBBgwFoAU
-    4tteG/vGAU1gbQ6s5O31sRKyQzkwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0B
-    AQsFAAOCAQEADz1sh6ipSNfbd3rE7hPRG1TN03Atmlq8yJ3uTsAGB7BDmPlad4fq
-    Ak+MrHuqDu0+iYEaOk/qcr0kP6ozBoorahA5GXO+qV1I9YlZH5dccC9RnffyksKT
-    T2epsJL91b9rh5bhUh4pBnQsRdpX8A7479DHNUP0SNYtrCW/vaeJuVbh9zU/VrLQ
-    vZ6Nk04h4X00M10C1v0hCFRJqKPXpUW96SJAmrM7F5TAiMFeO6zeGCoXWpaq7M5X
-    HwORa5h8YfdSozsB2XCDVd/0euDa5BOCBM5O431PhwL3peAwEeaGlXEk1XXnLDQ5
-    ylufmUaJuk6YltROWkNq/821NljZR5j4Nw==
-    -----END CERTIFICATE-----
+      -----BEGIN CERTIFICATE-----
+      MIIDFTCCAf2gAwIBAgIUOHwthMO3Dw04xRtE3/P9tzcGAc4wDQYJKoZIhvcNAQEL
+      BQAwGTEXMBUGA1UEAwwOMTkyLjE2OC4xMDAuMTAwIBcNMjQxMDE3MTM1NTU3WhgP
+      MzAyNDAyMTgxMzU1NTdaMBkxFzAVBgNVBAMMDjE5Mi4xNjguMTAwLjEwMIIBIjAN
+      BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAobtxgNLyzT1KjhVNmAJil0b4AxhV
+      xfRO5/QvF+ehykVKdB4C8yNv2PSjYA6JDflmYq0if0DoA0fW4znGem4/2wbRL+B/
+      dGiGUTvWnVv6sdPy7kArS8Q+z6b4R56VnDYvqN6N6ADLnCChur0rYu3F0H+vezZx
+      JRRg44Lzpw6KesyrE2YSd8vsdNjMJgu+35RZm91pZxrVzeQyHongTdQRtuabUq55
+      FyXvflbeAwpRTKAXGMxBXsZ7NgsCVm5EX9O0M0tw2Vy3TVAvh4rFAN89t7kIbsOx
+      EAMcMlmTqPr8rvoshh4w0mTP45ON489799/o5vLCLLYHoNkiwZDNbx48twIDAQAB
+      o1MwUTAdBgNVHQ4EFgQU4tteG/vGAU1gbQ6s5O31sRKyQzkwHwYDVR0jBBgwFoAU
+      4tteG/vGAU1gbQ6s5O31sRKyQzkwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0B
+      AQsFAAOCAQEADz1sh6ipSNfbd3rE7hPRG1TN03Atmlq8yJ3uTsAGB7BDmPlad4fq
+      Ak+MrHuqDu0+iYEaOk/qcr0kP6ozBoorahA5GXO+qV1I9YlZH5dccC9RnffyksKT
+      T2epsJL91b9rh5bhUh4pBnQsRdpX8A7479DHNUP0SNYtrCW/vaeJuVbh9zU/VrLQ
+      vZ6Nk04h4X00M10C1v0hCFRJqKPXpUW96SJAmrM7F5TAiMFeO6zeGCoXWpaq7M5X
+      HwORa5h8YfdSozsB2XCDVd/0euDa5BOCBM5O431PhwL3peAwEeaGlXEk1XXnLDQ5
+      ylufmUaJuk6YltROWkNq/821NljZR5j4Nw==
+      -----END CERTIFICATE-----
     ''
   ];
 }
