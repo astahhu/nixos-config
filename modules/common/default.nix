acd4d87861ad8a8a4aefaa9d9b46668c947219ee
florian <florian.schubert.sg@gmail.com>
2025-02-07 17:05:42 +0100

    fix: common

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -8,6 +8,7 @@
   options.astahhu.common = {
     is_server = lib.mkEnableOption "Enables default server settings";
     is_qemuvm = lib.mkEnableOption "Set if this server is a qemuvm, this will setup the default hardware config";
+    uses_btrfs = lib.mkEnableOption "Does this Server use a btrfs Filesystem (Snapshots and Backups)";
     disko.enable = lib.mkEnableOption "Use disko for disk management";
     disko.device = lib.mkOption {
       type = lib.types.str;
@@ -23,33 +24,23 @@
     {
       astahhu = {
         cli.better-tools.enable = true;
+        common.uses_btrfs = lib.mkDefault config.astahhu.common.disko.enable;
+      };
+      time.timeZone = "Europe/Berlin";
+      i18n.defaultLocale = "en_US.UTF-8";
+      console = {
+        keyMap = "us";
       };
 
-      nix-tun.storage.persist = lib.mkIf config.astahhu.common.is_server {
+
+      networking.firewall = {
         enable = true;
-        is_server = true;
       };
 
       nix.settings.trusted-users = [ "root" "@wheel" ];
 
-      services.openssh.enable = lib.mkIf config.astahhu.common.is_server true;
-      security.pam.sshAgentAuth.enable = lib.mkIf config.astahhu.common.is_server true;
+      common.uses_btrfs = config.astahhu.common.disko.enable;
 
-      services.btrbk.sshAccess = lib.mkIf config.astahhu.common.is_server [
-        {
-          key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPGx5yVTgRy/oXLuGvsK9PTr0hHbUCLz/+cKukb+L5K btrbk@asta-backup";
-          roles = [
-            "info"
-            "source"
-            "target"
-          ];
-        }
-      ];
-      users.users = lib.mkIf config.astahhu.common.is_server
-        {
-          btrbk.extraGroups = [ "wheel" ];
-          root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPGx5yVTgRy/oXLuGvsK9PTr0hHbUCLz/+cKukb+L5K root@asta-backup" ];
-        };
     }
     (lib.mkIf config.astahhu.common.disko.enable (import ./disko.nix {
       device = config.astahhu.common.disko.device;
