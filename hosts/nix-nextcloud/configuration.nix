792863a7437ffab1290f4a5f4f5d849dffd8e620
florian <florian.schubert.sg@gmail.com>
2025-03-07 16:04:02 +0100

    nextcloud: forced resolv.conf as disabled in container

--- hosts/nix-nextcloud/configuration.nix
+++ hosts/nix-nextcloud/configuration.nix
@@ -132,7 +132,7 @@
 
       users.users.nextcloud.extraGroups = [ "docker" ];
       networking.useHostResolvConf = lib.mkForce false;
-      environment.etc."resolv.conf" = lib.mkForce null;
+      environment.etc."resolv.conf".enable = lib.mkForce false;
 
       services.nginx.virtualHosts."cloud.astahhu.de".extraConfig = lib.mkForce ''
         index index.php index.html /index.php$request_uri;
