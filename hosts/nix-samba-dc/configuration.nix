e32d2f32b57e5cb775d11c8e1fafaa452f34885f
Florian Schubert <30634234+JamesofScout@users.noreply.github.com>
2025-08-28 22:12:35 +0200

    Update configuration.nix

--- hosts/nix-samba-dc/configuration.nix
+++ hosts/nix-samba-dc/configuration.nix
@@ -125,6 +125,65 @@
         ];
         pool = "134.99.154.10 - 134.99.154.80";
       };
+      domain-dfs = {
+        "intern" = {
+          "Intern AntiFaRaDis" = "nix-samba-fs.ad.astahhu.de/Intern AntiFaRaDis";
+          "Intern Autonom" = "nix-samba-fs.ad.astahhu.de/Intern Autonom";
+          "Intern Barriereref" = "nix-samba-fs.ad.astahhu.de/Intern Barriereref";
+          "Intern BiSchwu" = "nix-samba-fs.ad.astahhu.de/Intern BiSchwu";
+          "Intern Buchhaltung" = "nix-samba-fs.ad.astahhu.de/Intern Buchhaltung";
+          "Intern Deutschkurse" = "nix-samba-fs.ad.astahhu.de/Intern Deutschkurse";
+          "Intern Fachschaftsref" = "nix-samba-fs.ad.astahhu.de/Intern Fachschaftsref";
+          "Intern Finanzref" = "nix-samba-fs.ad.astahhu.de/Intern Finanzref";
+          "Intern Flüchtlingsarbeit" = "nix-samba-fs.ad.astahhu.de/Intern Flüchtlingsarbeit";
+          "Intern Frauenref" = "nix-samba-fs.ad.astahhu.de/Intern Frauenref";
+          "Intern HoPoref" = "nix-samba-fs.ad.astahhu.de/Intern HoPoref";
+          "Intern Internationalesref" = "nix-samba-fs.ad.astahhu.de/Intern Internationalesref";
+          "Intern ITref" = "nix-samba-fs.ad.astahhu.de/Intern ITref";
+          "Intern Kulturref" = "nix-samba-fs.ad.astahhu.de/Intern Kulturref";
+          "Intern Kommref" = "nix-samba-fs.ad.astahhu.de/Intern Kommref";
+          "Intern LesBiref" = "nix-samba-fs.ad.astahhu.de/Intern LesBiref";
+          "Intern Material" = "nix-samba-fs.ad.astahhu.de/Intern Material";
+          "Intern Materialbeauftragter" = "nix-samba-fs.ad.astahhu.de/Intern Materialbeauftragter";
+          "Intern Mieterverein" = "nix-samba-fs.ad.astahhu.de/Intern Mieterverein";
+          "Intern Oekoref" = "nix-samba-fs.ad.astahhu.de/Intern Oekoref";
+          "Intern Presseref" = "nix-samba-fs.ad.astahhu.de/Intern Presseref";
+          "Intern Rechtsberatung" = "nix-samba-fs.ad.astahhu.de/Intern Rechtsberatung";
+          "Intern Sekretariat Finanz Buchhaltung" = "nix-samba-fs.ad.astahhu.de/Intern Sekretariat Finanz Buchhaltung";
+          "Intern Sozialref" = "nix-samba-fs.ad.astahhu.de/Intern Sozialref";
+          "Intern SP" = "nix-samba-fs.ad.astahhu.de/Intern SP";
+          "Intern Steuern" = "nix-samba-fs.ad.astahhu.de/Intern Steuern";
+          "Intern Teamassistenz" = "nix-samba-fs.ad.astahhu.de/Intern Teamassistenz";
+          "Intern Vorstand" = "nix-samba-fs.ad.astahhu.de/Intern Vorstand";
+        };
+        "public" = {
+          "Public AntiFaRaDis" = "nix-samba-fs.ad.astahhu.de/Public AntiFaRaDis";
+          "Public Autonom" = "nix-samba-fs.ad.astahhu.de/Public Autonom";
+          "Public Barriereref" = "nix-samba-fs.ad.astahhu.de/Public Barriereref";
+          "Public BiSchwu" = "nix-samba-fs.ad.astahhu.de/Public BiSchwu";
+          "Public Deutschkurse" = "nix-samba-fs.ad.astahhu.de/Public Deutschkurse";
+          "Public Fachschaftsref" = "nix-samba-fs.ad.astahhu.de/Public Fachschaftsref";
+          "Public Finanzref" = "nix-samba-fs.ad.astahhu.de/Public Finanzref";
+          "Public Flüchtlingsarbeit" = "nix-samba-fs.ad.astahhu.de/Public Flüchtlingsarbeit";
+          "Public Frauenref" = "nix-samba-fs.ad.astahhu.de/Public Frauenref";
+          "Public HoPoref" = "nix-samba-fs.ad.astahhu.de/Public HoPoref";
+          "Public Internationalesref" = "nix-samba-fs.ad.astahhu.de/Public Internationalesref";
+          "Public ITref" = "nix-samba-fs.ad.astahhu.de/Public ITref";
+          "Public Kommref" = "nix-samba-fs.ad.astahhu.de/Public Kommref";
+          "Public Kulturref" = "nix-samba-fs.ad.astahhu.de/Public LesBiref";
+          "Public Lesbiref" = "nix-samba-fs.ad.astahhu.de/Public Kulturref";
+          "Public Materialbeauftragter" = "nix-samba-fs.ad.astahhu.de/Public Materialbeauftragter";
+          "Public Mieterverein" = "nix-samba-fs.ad.astahhu.de/Public Mieterverein";
+          "Public Oekoref" = "nix-samba-fs.ad.astahhu.de/Public Oekoref";
+          "Public Praesidium" = "nix-samba-fs.ad.astahhu.de/Public Oekoref";
+          "Public Presseref" = "nix-samba-fs.ad.astahhu.de/Public Praesidium";
+          "Public Rechtsberatung" = "nix-samba-fs.ad.astahhu.de/Public Rechtsberatung";
+          "Public Sekretariat Finanz Buchhaltung" = "nix-samba-fs.ad.astahhu.de/Public Sekretariat Finanz Buchhaltung";
+          "Public Sozialref" = "nix-samba-fs.ad.astahhu.de/Public Sozialref";
+          "Public Teamassistenz" = "nix-samba-fs.ad.astahhu.de/Public Teamassistenz";
+          "Public Vorstand" = "nix-samba-fs.ad.astahhu.de/Public Vorstand";
+        };
+      };
       dns = {
         dnssec-validation = "no";
         forwarders = [
