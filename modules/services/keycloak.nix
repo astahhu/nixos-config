03627634e3dc4ab0fd7e96c16e19edc128762d8b
Florian Schubert <30634234+JamesofScout@users.noreply.github.com>
2025-11-10 16:41:49 +0100

    Update keycloak.nix

--- modules/services/keycloak.nix
+++ modules/services/keycloak.nix
@@ -37,7 +37,8 @@
             http-port = 80;
             http-enabled = true;
             https-enabled = false;
-            hostname = "https://" + config.astahhu.services.keycloak.domain;
+            proxy-headers = true;
+            hostname = config.astahhu.services.keycloak.domain;
           };
           initialAdminPassword = "uekoajaeRae0eegh0phee9phohx6ahp8aangai8sae1Thun2xai5Hah3vee7Ooje";
           database = {
