3578ed5bd81375aff5d112ff411e521507bc78e5
MarchCraft <felix@dienilles.de>
2026-02-25 15:30:39 +0100

    blub

--- modules/services/samba/default.nix
+++ modules/services/samba/default.nix
@@ -4,173 +4,187 @@
   pkgs,
   ...
 }:
-{
+let
+  cfg = config.astahhu.services.samba;
+in
 
+{
   imports = [
     ./dc.nix
     ./fileserver.nix
   ];
 
   options.astahhu.services.samba = {
-    enable = lib.mkEnableOption "enables the samba module";
+    enable = lib.mkEnableOption "Enables the Samba module";
+
     hostname = lib.mkOption {
       type = lib.types.str;
-      description = "The netbios name of the host, defaults to hostname, can not be longer than 15 characters";
-      defaultText = "lib.strings.toUpper config.networking.hostName";
+      default = lib.strings.toUpper config.networking.hostName;
+      description = "NetBIOS name (max 15 characters).";
     };
+
     server_address = lib.mkOption {
       type = lib.types.str;
-      description = "The ip addresses under which this server is reachable";
       default = "";
+      description = "IP address the server is reachable at.";
     };
+
     package = lib.mkPackageOption pkgs "samba4Full" { };
+
     domain = lib.mkOption {
       type = lib.types.str;
-      description = "The Kerberos/AD Domain";
-      defaultText = "config.networking.domain";
+      default = config.networking.domain;
+      description = "Kerberos / AD domain.";
     };
+
     workgroup = lib.mkOption {
       type = lib.types.str;
-      description = "Sets the windows workgroup, has a maximum length of 15 characters";
       default = "WORKGROUP";
+      description = "Windows workgroup (max 15 chars).";
     };
+
     wsdd = lib.mkOption {
       type = lib.types.bool;
-      description = "Enables network discovery for this host";
       default = true;
+      description = "Enable network discovery.";
     };
+
     acme = {
-      enable = lib.mkEnableOption "Enables the creation of acme certificates for samba, currently only via cloudflare";
+      enable = lib.mkEnableOption "Enable ACME certificates for Samba";
+
       email = lib.mkOption {
         type = lib.types.str;
-        description = "The Email to be used for acme";
+        description = "Email for ACME.";
       };
     };
   };
 
   config =
-    let
-      cfg = config.astahhu.services.samba;
-    in
-    lib.mkIf cfg.enable {
-      nixpkgs.overlays = [
-        (final: prev: {
-          samba4Full = prev.samba4Full.overrideAttrs {
-            pythonPath = prev.samba4Full.pythonPath ++ [
-              prev.python3Packages.cryptography
-            ];
+    lib.mkIf config.astahhu.services.samba.enable
+
+      {
+
+        ########################################
+        # ZFS Persistence (REPLACES subvolumes)
+        ########################################
+
+        nix-tun.storage.persist.datasets = {
+
+          samba = {
+            backup = true;
+            path = "${config.nix-tun.storage.persist.path}/samba";
+            bindMountDirectories = true;
+
+            directories = {
+              "/var/lib/samba" = { };
+              "/var/lock/samba" = { };
+              "/var/lib/samba/private" = { };
+            };
           };
-        })
-      ];
 
-      astahhu.services.samba = {
-        hostname = lib.mkOptionDefault (lib.strings.toUpper config.networking.hostName);
-        domain = lib.mkOptionDefault config.networking.domain;
-      };
+          acme = lib.mkIf cfg.acme.enable {
+            backup = false;
+            path = "${config.nix-tun.storage.persist.path}/acme";
+            directories = {
+              "/var/lib/acme" = {
+                owner = "acme";
+                group = "acme";
+              };
+            };
+          };
 
-      nix-tun.storage.persist.subvolumes = {
-        samba = {
-          bindMountDirectories = true;
-          directories = {
-            "/var/lib/samba" = { };
-            "/var/lock/samba" = { };
+        };
+
+        ########################################
+        # Samba Overlay (cryptography fix)
+        ########################################
+
+        nixpkgs.overlays = [
+          (final: prev: {
+            samba4Full = prev.samba4Full.overrideAttrs {
+              pythonPath = prev.samba4Full.pythonPath ++ [ prev.python3Packages.cryptography ];
+            };
+          })
+        ];
+
+        ########################################
+        # ACME TLS Handling
+        ########################################
+
+        security.acme = lib.mkIf cfg.acme.enable {
+          acceptTerms = true;
+
+          certs.samba = {
+            email = cfg.acme.email;
+            domain = "${lib.strings.toLower cfg.hostname}.${cfg.domain}";
+            dnsProvider = "cloudflare";
+            environmentFile = config.sops.secrets.cloudflare-dns.path;
+            group = "root";
+
+            postRun = ''
+              systemctl restart samba.target
+            '';
           };
         };
-        acme = {
-          owner = "acme";
-          bindMountDirectories = true;
-          backup = false;
-          directories."/var/lib/acme" = { };
-        };
 
-      };
+        systemd.services.samba-tls = lib.mkIf cfg.acme.enable {
+          serviceConfig.Type = "oneshot";
 
-      security.acme = lib.mkIf cfg.acme.enable {
-        acceptTerms = true;
-        certs.samba = {
-          email = cfg.acme.email;
-          domain = "${lib.strings.toLower cfg.hostname}.${cfg.domain}";
-          dnsResolver = "134.99.128.2";
-          dnsProvider = "cloudflare";
-          extraLegoFlags = [
-            #"-dns.propagation-disable-ans=true"
-            #"--dns.propagation-rns=true"
-            "--dns.propagation-wait=600s"
-          ];
-          dnsPropagationCheck = true;
-          group = "root";
-          environmentFile = config.sops.secrets.cloudflare-dns.path;
-          postRun = lib.mkIf cfg.acme.enable ''
-            systemctl restart samba.target
+          script = ''
+            install -m 600 -o root -g root \
+              /var/lib/acme/samba/key.pem \
+              /var/lib/samba/private/tls/key.pem
+
+            install -m 600 -o root -g root \
+              /var/lib/acme/samba/cert.pem \
+              /var/lib/samba/private/tls/cert.pem
           '';
-        };
-      };
 
-      systemd.services.samba-tls = lib.mkIf cfg.acme.enable {
-        serviceConfig = {
-          Type = "oneshot";
+          requires = [ "acme-samba.service" ];
+          before = [ "samba.target" ];
         };
 
-        script = ''
-          cp /var/lib/acme/samba/key.pem  /var/lib/samba/private/tls/key.pem
-          cp /var/lib/acme/samba/cert.pem /var/lib/samba/private/tls/cert.pem
-          chmod 600 /var/lib/samba/private/tls/key.pem
-          chmod 600 /var/lib/samba/private/tls/cert.pem 
-          chown root:root  /var/lib/samba/private/tls/key.pem
-          chown root:root  /var/lib/samba/private/tls/cert.pem  
-        '';
+        ########################################
+        # Kerberos
+        ########################################
 
-        requires = lib.mkIf cfg.acme.enable [
-          "acme-samba.service"
-        ];
-
-        before = [
-          "samba.target"
-        ];
-      };
-
-      security.krb5 = {
-        enable = true;
-        settings = {
-          libdefaults = {
-            default_realm = lib.strings.toUpper cfg.domain;
-            dns_lookup_realm = false;
-            dns_lookup_kdc = true;
-          };
-          realms = {
-            "${lib.strings.toUpper cfg.domain}" = {
-              "default_domain" = cfg.domain;
-            };
-
-            "${cfg.workgroup}" = {
-              "default_domain" = cfg.domain;
+        security.krb5 = {
+          enable = true;
+          settings = {
+            libdefaults = {
+              default_realm = lib.strings.toUpper cfg.domain;
+              dns_lookup_realm = false;
+              dns_lookup_kdc = true;
             };
           };
-          "domain_realm" = {
-            "${cfg.hostname}" = lib.strings.toUpper cfg.domain;
-          };
         };
-      };
 
-      services.samba-wsdd = lib.mkIf cfg.wsdd {
-        enable = true;
-        openFirewall = true;
-        domain = cfg.domain;
-        hoplimit = 4;
-      };
+        ########################################
+        # WSDD
+        ########################################
 
-      services.samba.settings.global = {
-        "allow dcerpc auth level connect" = "no";
-        "netbios name" = cfg.hostname;
-        "restrict anonymous" = 2;
-        "log level" = "0";
-        "disable netbios" = "yes";
-        "realm" = cfg.domain;
-        "workgroup" = cfg.workgroup;
-        "tls keyfile" = "tls/key.pem";
-        "tls certfile" = "tls/cert.pem";
-        "tls cafile" = if cfg.acme.enable then "" else "tls/ca.pem";
-      };
-    };
+        services.samba-wsdd = lib.mkIf cfg.wsdd {
+          enable = true;
+          openFirewall = true;
+          domain = cfg.domain;
+        };
 
+        ########################################
+        # Global Samba Settings
+        ########################################
+
+        services.samba.settings.global = {
+          "netbios name" = cfg.hostname;
+          "realm" = cfg.domain;
+          "workgroup" = cfg.workgroup;
+
+          "disable netbios" = "yes";
+          "restrict anonymous" = 2;
+          "log level" = "0";
+
+          "tls keyfile" = "private/tls/key.pem";
+          "tls certfile" = "private/tls/cert.pem";
+        };
+
+      };
 }
